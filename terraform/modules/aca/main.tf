0415bf9d80674380faec5ca391650e3ab2c90da6
medkhalil <medkha.bt@gmail.com>
2026-09-23 23:36:54 +0200

    Add a vnet and link a subnet of the vnet to the azure container app env, create another vnet for the private links

--- terraform/modules/aca/main.tf
+++ terraform/modules/aca/main.tf
@@ -21,6 +21,44 @@ resource "azurerm_resource_group" "demo_aca" {
   location = "North Europe"
 }
 
+resource "azurerm_virtual_network" "demo-vnet" {
+  name = "vnet-aca-demo-${var.enviroment}"
+  location = azurerm_resource_group.demo_aca.location
+  resource_group_name = azurerm_resource_group.demo_aca.name
+
+  address_space = ["10.0.0.0/26"]
+
+}
+
+resource "azurerm_subnet" "subnet-aca" {
+    name = "subnet-aca-${var.enviroment}"
+    resource_group_name = azurerm_resource_group.demo_aca.name
+    virtual_network_name = azurerm_virtual_network.demo-vnet.name
+
+    address_prefixes = ["10.0.0.0/27"]
+
+    delegation {
+      name = "aca-delegation"
+
+      service_delegation {
+        name = "Microsoft.App/environments"
+
+        actions = [
+          "Microsoft.Network/virtualNetworks/subnets/join/action"
+        ]
+      }
+      
+    }
+}
+
+resource "azurerm_subnet" "subnet-private-endpoints" {
+    name = "subnet-private-endpoints-${var.enviroment}"
+    resource_group_name = azurerm_resource_group.demo_aca.name
+    virtual_network_name = azurerm_virtual_network.demo-vnet.name
+
+    address_prefixes = ["10.0.0.32/27"]
+}
+
 resource "azurerm_log_analytics_workspace" "demo_aca" {
   name                = "law-terraform-demo-aca-${var.enviroment}"
   location            = azurerm_resource_group.demo_aca.location
@@ -35,6 +73,8 @@ resource "azurerm_container_app_environment" "demo_aca" {
   resource_group_name        = azurerm_resource_group.demo_aca.name
   logs_destination           = "log-analytics"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.demo_aca.id
+  infrastructure_subnet_id  = azurerm_subnet.subnet-aca.id
+
   workload_profile {
     maximum_count         = 0
     minimum_count         = 0
